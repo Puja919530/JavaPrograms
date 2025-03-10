0854950f2dfbe08af58dca124937e9d630e84575
Aniket & Puja <pujakatekhayep@gmail.com>
2025-03-10 13:47:29 +0530

    10th march - commit

---
+++ src/ex_01_Java_Basic/Lab001_Comments.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+public class Lab001_Comments {
+
+    public static void main(String[] args){
+
+//        1.Singlie Line Comment
+//        This is single line comment.
+//        we use comment for the Explanation, description or any relevant information about the code.
+
+        /*2.Multiple line comments*/
+        /*
+        This is Multiple-line comment.
+        Java compiler ignores everything in this block
+        */
+    }
+}
