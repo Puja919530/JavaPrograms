1939e58d6263b86c21a11caa72a5fa18e4522b78
Aniket & Puja <pujakatekhayep@gmail.com>
2025-03-10 14:13:22 +0530

    10th march - commit

---
+++ src/ex_01_Java_Basic/Lab004_Println_Vs_Print.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab004_Println_Vs_Print {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        System.out.print("Hello World");
+        System.out.print("Hello World");
+    }
+}
