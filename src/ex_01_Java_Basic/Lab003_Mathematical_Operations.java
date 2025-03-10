240d6d57aaab839b2c62d9704aa0f15feb53324f
Aniket & Puja <pujakatekhayep@gmail.com>
2025-03-10 14:08:01 +0530

    10th march - commit

---
+++ src/ex_01_Java_Basic/Lab003_Mathematical_Operations.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basic;
+
+public class Lab003_Mathematical_Operations
+{
+    public static void main(String[] args){
+        System.out.println("Addition- " + (2+3));
+        System.out.println("Substration-" + (5-2));
+        System.out.println("Multiplication- " + (2*2));
+        System.out.println("Divition- " + (2/2));
+        System.out.println("Modulus- " + 5%2);
+
+        //Throw Arithmetic Exception
+        System.out.println("Divition- " + (10/0));
+
+    }
+}
