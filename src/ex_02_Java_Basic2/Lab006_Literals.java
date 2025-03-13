8cc74200def1a5ae6c11fc9386ce4659c8b1ce4c
Aniket & Puja <pujakatekhayep@gmail.com>
2025-03-13 21:22:11 +0530

    13th march - commit

---
+++ src/ex_02_Java_Basic2/Lab006_Literals.java
@@ -0,0 +1,29 @@
+package ex_02_Java_Basic2;
+
+public class Lab006_Literals {
+    public static void main(String[] args) {
+
+        int age = 65; //65 is literal. Actual value is literals
+
+        //Binary literals
+        int binary_num = 0b1010;
+
+        //Octal literals
+        int octal = 0101;
+
+        // hexadecimla literals
+        int hexa = 0xface;
+
+//        character literals
+        char c = 'A';
+        char c1 = 'B';
+        char c2 = '@';
+        char c3 = '2';
+        char c4 = ' ';
+        char c5 = 'a';
+        char c6 = '$';
+        System.out.println(c4);// print space on console
+        System.out.println(c3);
+        System.out.println(c5);
+    }
+}
