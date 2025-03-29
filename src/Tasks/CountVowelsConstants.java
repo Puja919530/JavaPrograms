cdd0126a2dde12d3450b4bdc496cd9ec70bf5b97
Aniket & Puja <pujakatekhayep@gmail.com>
2025-03-29 18:19:58 +0530

    29th march - commit

--- src/Tasks/CountVowelsConstants.java
+++ src/Tasks/CountVowelsConstants.java
@@ -2,7 +2,7 @@ package Tasks;
 
 import java.util.Scanner;
 
-public class CountVowelConstant {
+public class CountVowelsConstants {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the string");
