ef313851ee6416b55d26d38f0104002b95452d52
Aniket & Puja <pujakatekhayep@gmail.com>
2025-04-02 17:28:01 +0530

    2st April - commit

---
+++ src/ex_02_Java_Basic2/Lab005_Variables.java
@@ -0,0 +1,25 @@
+package ex_02_Java_Basic2;
+
+public class Lab005_Variables {
+    public static void main(String[] args) {
+        //1. int
+         int a = 10, b = 23, c = 90;
+//         int _ = 10;
+        int local;
+        local = 10;
+        System.out.println(local);
+
+//        2.float
+        final float pi = 3.14f;
+        System.out.println(pi);
+
+//        3.boolean
+        boolean is_married = true;
+
+        boolean is_married_puja = false;
+
+        System.out.println(is_married_puja);
+        System.out.println(is_married);
+
+    }
+}
---
+++ src/ex_05_string/StringFunction.java
@@ -0,0 +1,8 @@
+package ex_05_string;
+
+public class StringFunction {
+    public static void main(String[] args) {
+
+        String s = "puja"
+    }
+}
