cf5f22b758575318169c089cf5bdabb82caee769
Aniket & Puja <pujakatekhayep@gmail.com>
2025-04-03 23:55:54 +0530

    3rd April - commit

---
+++ src/ex_03_coding_tasks/WhatWillBeTheOutput.java
@@ -0,0 +1,17 @@
+package ex_03_coding_tasks;
+
+public class WhatWillBeTheOutput {
+    public static void main(String[] args) {
+//        int a = 20;
+//        System.out.println(--a + a++ + a--);
+
+        // ans is 58
+        int a = 10;
+        System.out.println(++a + a++ + a++);
+        System.out.println(a);
+
+        //ans  34
+        //     13
+    }
+}
+
