b448a329d8aedaf024af3403951c5aa976dd4b43
Aniket & Puja <pujakatekhayep@gmail.com>
2025-04-13 23:58:55 +0530

    9th April - commit

---
+++ src/ex_18_Wrapper_Class/Lab_040_Wrapper.java
@@ -0,0 +1,21 @@
+package ex_18_Wrapper_Class;
+
+class ATB1 {
+    String name;
+    Long phone;
+    Integer age;
+    Double salary;
+    Float GST;
+    Boolean isMarried;
+
+    // int -> data types
+    // Integer -> Object -> attribute, behavriour
+    // In future -> to convert the age -> string, primitive to hextValue... else you can now do it.
+
+}
+
+public class Lab_040_Wrapper {
+    public static void main(String[] args) {
+
+    }
+}
